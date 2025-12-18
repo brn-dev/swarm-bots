facfeeb373195e8303f9380cb6eddbff8e491e77
Brn <dominik.b4ron@gmail.com>
2025-12-18 22:56:27 +0100

    .

---
+++ swarmbots/learn/nn_components/nn_init.py
@@ -0,0 +1,10 @@
+from typing import Callable
+
+from torch import nn
+
+LinearInitialization = Callable[[nn.Linear], None]
+
+def init_linear_orthogonal(module: nn.Linear, gain: float = 0.01) -> nn.Linear:
+    nn.init.orthogonal_(module.weight, gain=gain)
+    nn.init.constant_(module.bias, 0)
+    return module
