55f6ba05035b8f3fd89f536c340f19b6f919265b
Brn <dominik.b4ron@gmail.com>
2026-06-01 18:27:21 +0200

    .

--- swarmbots/learn/algos/base_algorithm.py
+++ swarmbots/learn/algos/base_algorithm.py
@@ -32,7 +32,7 @@ from swarmbots.utils.machine_specs import collect_machine_specs
 
 
 
-MIN_ITERATIONS_FOR_EMA = 150
+MIN_ITERATIONS_FOR_EMA = 10
 MIN_ITERATIONS_FOR_BEST = 200
 
 LearningRate = float | list[float] | dict[str, float]
