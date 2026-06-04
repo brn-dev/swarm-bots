695520cc14e7df42d51bc2049407f0a733d474ae
Brn <dominik.b4ron@gmail.com>
2026-06-04 20:42:08 +0200

    .

--- experiments/mat_nop_mjw_wall_common.py
+++ experiments/mat_nop_mjw_wall_common.py
@@ -652,7 +652,7 @@ def run_experiment(
             ("ep_rew", SummaryStatisticsFormat(mean=" .2f", std=".2f", max_value=" .2f", n="1")),
             ("ep_rew_ema", " .3f"),
             ("best_ep_rew_ema", " .3f", "best_ema"),
-            ("ep_success_rate_ema", "4.1f", "success_pct_ema"),
+            ("ep_success_rate_ema", "5.2f", "success_pct_ema"),
             ("fps", None),
         ]
     )
