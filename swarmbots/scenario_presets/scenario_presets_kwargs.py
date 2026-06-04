6a39509270c537d6cd3df98d8fd6acccf821d6b3
Brn <dominik.b4ron@gmail.com>
2026-06-04 18:57:41 +0200

    .

--- swarmbots/scenario_presets/scenario_presets_kwargs.py
+++ swarmbots/scenario_presets/scenario_presets_kwargs.py
@@ -44,7 +44,7 @@ WALL_SCENARIO_KWARGS: dict[str, object] = {
     "no_initial_ramp": True,
     "wall_height": 0.20,
     "forward_reward_weight": 1.0,
-    "forward_reward_max_y": 1.5,
+    "forward_reward_max_y": 100.0,
     "forward_reward_wall_boost_factor": 1.0,
     "wall_pass_reward_weight": 10.0,
     "wall_pass_reward_skew": 0.0,
@@ -67,7 +67,7 @@ WALL_DIFFICULTY_UPDATES: dict[Difficulty, dict[str, object]] = {
         "wall_climb_reward_weight": 4.0,
         "wall_pass_reward_weight": 5.0,
         "wall_pass_reward_skew": 4.0,
-        "wall_pass_thresholds": [0.2, 0.4],
+        "wall_pass_thresholds": [0.2, 0.5],
         "units_without_connections_reward_weight": -3e-3,
     },
     "hard": {
